9d1106ab6f9544bf1eef4ff94d33683c9c2e3fd1
Mathias Davidheimann <41026754+dd5md@users.noreply.github.com>
2019-04-20 11:22:11 +0200

    Update config.rb

--- template/config.rb
+++ template/config.rb
@@ -1,46 +1,52 @@
-# Activate and configure extensions
-# https://middlemanapp.com/advanced/configuration/#configuring-extensions
+#-------------------------------------
+# https://dd5md.de Configuration
+#-------------------------------------
 
+#-------------------------------------
+#  Dir Helpers
+#-------------------------------------
+
+# Helpers Reload
+Dir['helpers/*'].each(&method(:load))
+
+#-------------------------------------
+#  Activate & Configure Extensions
+#-------------------------------------
+
+# i18n
+activate :i18n, mount_at_root: :de
+
+# Autoprefixer
 activate :autoprefixer do |prefix|
-  prefix.browsers = "last 2 versions"
+  prefix.browsers = 'last 4 versions'
+  prefix.cascade  = false
 end
 
-# Layouts
-# https://middlemanapp.com/basics/layouts/
+#-------------------------------------
+# Layout-specific Configuration
+#-------------------------------------
 
-# Per-page layout changes
-page '/*.xml', layout: false
-page '/*.json', layout: false
-page '/*.txt', layout: false
+# With no Layout
+[:xml, :txt, :json].each do |ext|
+  page '/*.' + ext.to_s, layout: false
+end
 
-# With alternative layout
-# page '/path/to/file.html', layout: 'other_layout'
+#-------------------------------------
+# Development-specific Configuration
+#-------------------------------------
 
-# Proxy pages
-# https://middlemanapp.com/advanced/dynamic-pages/
+configure :development do
+  # Livereload
+  activate :livereload do |reload|
+    reload.no_swf = true
+  end
+end
 
-# proxy(
-#   '/this-page-has-no-template.html',
-#   '/template-file.html',
-#   locals: {
-#     which_fake_page: 'Rendering a fake page with a local variable'
-#   },
-# )
+#-------------------------------------
+# External Pipeline
+#-------------------------------------
 
-# Helpers
-# Methods defined in the helpers block are available in templates
-# https://middlemanapp.com/basics/helper-methods/
-
-# helpers do
-#   def some_helper
-#     'Helping'
-#   end
-# end
-
-# Build-specific configuration
-# https://middlemanapp.com/advanced/configuration/#environment-specific-settings
-
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+activate :external_pipeline,
+  name: :gulp,
+  command: build? ? 'npm run gulp' : 'npm run watch',
+  source: '.tmp'
